d76305dcfa20debb8a91f209e73459909fe57d45
MickiPoulsen <mickihp@gmail.com>
2026-04-30 00:06:07 +0200

    added linding

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["*/.{js,jsx}"],
+    plugins: { react },
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    }
+  }
+];
